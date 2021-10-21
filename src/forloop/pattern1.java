0d7c589a2421cf036c94bc6becc9f86aab8baf75
shaiksha <shavali2009@gmail.com>
2021-10-21 19:03:52 +0530

    second attempt

---
+++ src/forloop/pattern1.java
@@ -0,0 +1,18 @@
+package forloop;
+
+public class pattern1 {
+	public static void main(String[] args)
+	{
+		int a=8;
+		for(int i=1;i<=a;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+}
